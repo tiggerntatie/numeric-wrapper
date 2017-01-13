f05a7b221a6f10728b21bb99a0166b5e5b832fdf
Eric Dennison <ericd@netdenizen.com>
2017-01-13 14:40:26 -0500

    Updated from Brython Server: 1/13/2017 2:40:26 PM

--- test.py
+++ test.py
@@ -3,4 +3,19 @@ class ONum(object):
     def __init__(self, val):
         self.val = val
         
-    
+    def __add__(self, y):
+        return ONum(self.val + y)
+        
+    def __radd__(self, x):
+        return x + self.val
+        
+    def __iadd__(self, y):
+        self.val += y
+        return self
+        
+x = ONum(5)
+print(x+3, type(x+3))
+print(3+x, type(3+x))
+
+x += 3
+print(x, type(x))

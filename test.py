acbdde54a3a9e513d249fac12a794b9deebff4b7
Eric Dennison <ericd@netdenizen.com>
2017-01-24 10:35:47 -0500

    Updated from Brython Server: 1/24/2017 10:35:46 AM

--- test.py
+++ test.py
@@ -1,29 +1,43 @@
 class ONum(object):
 
     def __init__(self, val):
-        self.val = val
+        self._val = lambda : val
         
     def __str__(self):
-        return str(self.val)
+        return str(self._val)
+        
+    @property
+    def val(self):
+        return self._val()
+        
+    @val.setter
+    def val(self, val):
+        self._val = lambda : val
         
     def __add__(self, y):
         #print("__add__")
-        return ONum(self.val + y)
+        self._val = lambda s = self, v = y : ONum(s.val + v)
+        return self.val
         
     def __radd__(self, x):
         #print("__radd__")
-        return ONum(x + self.val)
+        self._val = lambda s = self, v = x : ONum(x + s.val)
+        return self.val
         
     def __iadd__(self, y):
         #print("__iadd__")
         self.val += y
         return self
+
+
+        
         
 x = ONum(5)
 y = ONum(3)
 print(x+3, type(x+3)) # __add__
 print(3+x, type(3+x)) # __radd__
 
+"""
 x += 3  # __iadd__
 print(x, type(x))
 
@@ -38,3 +52,4 @@ print(id(z))
 z += x # iadd, radd
 print(z, type(z))
 print(id(z))
+"""

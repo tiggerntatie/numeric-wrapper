1f2cb982a264d2a4e4f8d129868cd8a2f46933a2
Eric Dennison <ericd@netdenizen.com>
2017-01-24 11:40:29 -0500

    Updated from Brython Server: 1/24/2017 11:40:27 AM

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class ONum(object):
 
     def __iadd__(self, y):
         print("__iadd__")
-        x = self + yval
+        x = self + y
         print("inside: ", x)
         self._valf = lambda yval = y: (self + yval)._valf
 
